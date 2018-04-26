c0f8524c5602722ab328a50a06b7058a70f10fc8
Clint Dunn <cdunn6754@gmail.com>
2018-04-26 16:57:34 -0600

    Moves the main_index to main templates directory

--- cdunnBlog/urls.py
+++ cdunnBlog/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from blogPost.views import blogIndex
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', blogIndex, name='blog_index'),
+    url(r'^$', TemplateView.as_view(template_name="static_html/main_index.html"), name='main_index'),
     url(r'^users/', include('blogUser.urls',namespace='blogUser')),
 ]
