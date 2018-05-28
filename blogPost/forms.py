5750e26ef108b7e82624e0243d459fa98b355355
Clint <cdunn6754@gmail.com>
2018-05-28 08:08:25 -0600

    Implements html file upload capability
    Previously I had to load the an html file that I converted myself with pandoc
    manually to the website files. Now the user can upload a file though the site.
    I guess the next step would be to let the user upload a markdown file and
    convert it on the site into html.

--- blogPost/forms.py
+++ blogPost/forms.py
@@ -1,9 +1,12 @@
 from django.forms import ModelForm
+from django import forms
 
 from blogPost.models import BlogPost
 
 class BlogPostForm(ModelForm):
-    template_name = "blogPost/blog_post_form/html"
+    template_name = "blogPost/blog_post_form.html"
+    temp_markdown_file = forms.FileField(label="Markdown File")
+
     class Meta:
         model = BlogPost
-        fields = [user_markdown]
+        fields = ['title']
--- blogPost/views.py
+++ blogPost/views.py
@@ -4,23 +4,34 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django import forms
 
 from blogPost.models import BlogPost
+from blogPost.forms import BlogPostForm
 
 # Create your views here.
-# def blogIndex(request):
-#     return render(request, "blogPost/blog_index.html")
 
 @method_decorator(login_required, name='dispatch')
 class BlogCreateView(CreateView):
     model = BlogPost
     template_name = "blogPost/blog_post_form.html"
 
-    fields = ['user_markdown', 'title']
+    #fields = ['user_markdown', 'title']
+    form_class = BlogPostForm
 
     def form_valid(self, form):
-        print("In form valid fcn")
         form.instance.author = self.request.user.BlogUser
+
+        # Break file into chunks if necessary to not overload available memory
+        if form.cleaned_data['temp_markdown_file'].multiple_chunks():
+            while True:
+                chunk = form.cleaned_data['temp_markdown_file'].chunk()
+                if chunk:
+                    form.instance.user_markdown += chunk
+                else:
+                    break
+        else:
+            form.instance.user_markdown = form.cleaned_data['temp_markdown_file'].read()
         return super().form_valid(form)
 
 class BlogPostDetailView(DetailView):
